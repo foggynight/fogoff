322bb991256c414a89d0754610ab2936e1e29c72
foggynight <foggynight709@gmail.com>
2020-08-10 05:01:10 -0230

    Add argument processing function
    Wrapped the command line argument processing in a function to clean
    up the main.c file.
    
    Also removed the external declaration for the key string, will be
    passing it to the encrypt and decrypt functions via pointer.

--- src/config.h
+++ src/config.h
@@ -1,4 +1,2 @@
 #define MAXKEYLEN       1024
 #define MAXKEYSTRLEN    MAXKEYLEN + 1
-
-extern char key[MAXKEYSTRLEN];
--- src/main.c
+++ src/main.c
@@ -14,77 +14,21 @@ int is_decrypt_option(char *str);
 int is_key_option(char *str);
 char *get_key(char *dest);
 
-struct {
+typedef struct {
 	unsigned int encrypt : 1;
 	unsigned int decrypt : 1;
 	unsigned int key_arg : 1;
-} flags;
+} Flags;
 
 enum { ENCRYPT, DECRYPT, KEY, INVALID };
 
-char key[MAXKEYSTRLEN]; // external
+char key[MAXKEYSTRLEN];
 
 int main(int argc, char **argv) {
-	static struct {
-		unsigned int encrypt : 1;
-		unsigned int decrypt : 1;
-		unsigned int key_arg : 1;
-	} flags;
+	char *get_key(char *dest);
+	Flags get_flags(int argc, char **argv);
 
-	for (char **arg_ptr = argv+1; argc > 1; --argc, ++arg_ptr) {
-		switch (option_type(*arg_ptr)) {
-		case ENCRYPT:
-			if (flags.decrypt) {
-				fprintf(stderr,
-					"%s: invalid use: encrypt and decrypt selected\n",
-					argv[0]);
-				return 1;
-			} else {
-				flags.encrypt = 1;
-			}
-			break;
-		case DECRYPT:
-			if (flags.encrypt) {
-				fprintf(stderr,
-					"%s: invalid use: encrypt and decrypt selected\n",
-					argv[0]);
-				return 1;
-			} else {
-				flags.decrypt = 1;
-			}
-			break;
-		case KEY:
-			if (flags.key_arg) {
-				fprintf(stderr,
-					"%s: invalid use: multiple keys detected\n",
-					argv[0]);
-				return 1;
-			} else if (--argc <= 1) {
-				fprintf(stderr,
-					"%s: invalid use: key option selected but no key provided\n",
-					argv[0]);
-				return 1;
-			} else {
-				++arg_ptr;
-				if (strlen(*arg_ptr) > MAXKEYLEN) {
-					fprintf(stderr,
-						"%s: invalid use: key too long, max length: %d\n",
-						argv[0], MAXKEYLEN);
-					return 1;
-				} else {
-					flags.key_arg = 1;
-					strcpy(key, *arg_ptr);
-				}
-			}
-			break;
-		default:
-			fprintf(stderr,
-				"%s: invalid argument: %s\n",
-				argv[0], *arg_ptr);
-			return 1;
-			break;
-		}
-	}
+	Flags flags = get_flags(argc, argv);
 
 	if (!flags.key_arg) {
 		if (!get_key(key)) {
@@ -110,6 +54,16 @@ int main(int argc, char **argv) {
 	return 0;
 }
 
+char *get_key(char *dest) {
+	FILE *key_file = fopen("key.txt", "r");
+	if (!key_file) return NULL;
+
+	fscanf(key_file, "%" str(MAXKEYLEN) "s", dest);
+
+	fclose(key_file);
+	return dest;
+}
+
 int is_encrypt_option(char *str) {
 	return strcmp(str, "-e") == 0 || strcmp(str, "--encrypt") == 0;
 }
@@ -129,12 +83,63 @@ char option_type(char *str) {
 	else return INVALID;
 }
 
-char *get_key(char *dest) {
-	FILE *key_file = fopen("key.txt", "r");
-	if (!key_file) return NULL;
+Flags get_flags(int argc, char **argv) {
+	Flags flags = {0};
 
-	fscanf(key_file, "%" str(MAXKEYLEN) "s", dest);
-	
-	fclose(key_file);
-	return dest;
+	for (char **arg_ptr = argv+1; argc > 1; --argc, ++arg_ptr) {
+		switch (option_type(*arg_ptr)) {
+		case ENCRYPT:
+			if (flags.decrypt) {
+				fprintf(stderr,
+					"%s: invalid use: encrypt and decrypt selected\n",
+					argv[0]);
+				exit(1);
+			} else {
+				flags.encrypt = 1;
+			}
+			break;
+		case DECRYPT:
+			if (flags.encrypt) {
+				fprintf(stderr,
+					"%s: invalid use: encrypt and decrypt selected\n",
+					argv[0]);
+				exit(1);
+			} else {
+				flags.decrypt = 1;
+			}
+			break;
+		case KEY:
+			if (flags.key_arg) {
+				fprintf(stderr,
+					"%s: invalid use: multiple keys detected\n",
+					argv[0]);
+				exit(1);
+			} else if (--argc <= 1) {
+				fprintf(stderr,
+					"%s: invalid use: key option selected but no key provided\n",
+					argv[0]);
+				exit(1);
+			} else {
+				++arg_ptr;
+				if (strlen(*arg_ptr) > MAXKEYLEN) {
+					fprintf(stderr,
+						"%s: invalid use: key too long, max length: %d\n",
+						argv[0], MAXKEYLEN);
+					exit(1);
+				} else {
+					flags.key_arg = 1;
+					strcpy(key, *arg_ptr);
+				}
+			}
+			break;
+		default:
+			fprintf(stderr,
+				"%s: invalid argument: %s\n",
+				argv[0], *arg_ptr);
+			exit(1);
+			break;
+		}
+	}
+
+	return flags;
 }
