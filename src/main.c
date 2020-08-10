dddfeb4bfd0013806229ebfb63e466c7fca54435
foggynight <foggynight709@gmail.com>
2020-08-09 23:50:53 -0230

    Add function to get encryption key
    Added function get_key to read the encryption key from the key.txt file.

--- src/main.c
+++ src/main.c
@@ -1,34 +1,56 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
-int encrypt_option(char *str);
-int decrypt_option(char *str);
+#define MAXKEYLEN    1024
+#define MAXSTRLEN    MAXKEYLEN + 1
+
+#define stringify(x) #x
+#define str(x) stringify(x)
+
+int is_encrypt_option(char *str);
+int is_decrypt_option(char *str);
+char *get_key(void);
 
 int main(int argc, char **argv) {
 	if (argc < 2) {
-		printf("%s: invalid use: too few arguments", argv[0]);
+		printf("%s: invalid use: too few arguments\n", argv[0]);
+		return 1;
 	} else if (argc > 2) {
-		printf("%s: invalid use: too many arguments", argv[0]);
+		printf("%s: invalid use: too many arguments\n", argv[0]);
+		return 1;
 	}
 
 	FILE *input_fp = stdin;
 	FILE *output_fp = stdout;
+	char *key = get_key();
 
-	if (encrypt_option(argv[1])) {
+	if (is_encrypt_option(argv[1])) {
 		// encrypt input
-	} else if (decrypt_option(argv[1])) {
+	} else if (is_decrypt_option(argv[1])) {
 		// decrypt input
 	} else {
-		printf("%s: invalid argument: %s", argv[0], argv[1]);
+		printf("%s: invalid argument: %s\n", argv[0], argv[1]);
 	}
 
 	return 0;
 }
 
-int encrypt_option(char *str) {
+int is_encrypt_option(char *str) {
 	return strcmp(str, "-e") == 0 || strcmp(str, "--encrypt") == 0;
 }
 
-int decrypt_option(char *str) {
+int is_decrypt_option(char *str) {
 	return strcmp(str, "-d") == 0 || strcmp(str, "--decrypt") == 0;
+}
+
+char *get_key(void) {
+	FILE *key_file = fopen("key.txt", "r");
+	if (!key_file) return NULL;
+
+	char *key = calloc(MAXSTRLEN, sizeof(char));
+	fscanf(key_file, "%" str(MAXKEYLEN) "s", key);
+	
+	fclose(key_file);
+	return key;
 }
