1c62835f1d731b6282cc7eaccc155b81fcf5d46b
foggynight <foggynight709@gmail.com>
2020-08-10 03:12:25 -0230

    Update the command line argument loop
    Changed out the chained ifs of the command line argument loop for a
    switch statement.

--- src/main.c
+++ src/main.c
@@ -20,50 +20,77 @@ struct {
 	unsigned int key_arg : 1;
 } flags;
 
-char key[MAXKEYSTRLEN];
+enum { ENCRYPT, DECRYPT, KEY, INVALID };
+
+char key[MAXKEYSTRLEN]; // external
 
 int main(int argc, char **argv) {
+	static struct {
+		unsigned int encrypt : 1;
+		unsigned int decrypt : 1;
+		unsigned int key_arg : 1;
+	} flags;
+
 	for (char **arg_ptr = argv+1; argc > 1; --argc, ++arg_ptr) {
-		if (is_encrypt_option(*arg_ptr)) {
+		switch (option_type(*arg_ptr)) {
+		case ENCRYPT:
 			if (flags.decrypt) {
-				fprintf(stderr, "%s: invalid use: encrypt and decrypt selected\n", argv[0]);
+				fprintf(stderr,
+					"%s: invalid use: encrypt and decrypt selected\n",
+					argv[0]);
 				return 1;
 			} else {
 				flags.encrypt = 1;
 			}
-		} else if (is_decrypt_option(*arg_ptr)) {
+			break;
+		case DECRYPT:
 			if (flags.encrypt) {
-				fprintf(stderr, "%s: invalid use: encrypt and decrypt selected\n", argv[0]);
+				fprintf(stderr,
+					"%s: invalid use: encrypt and decrypt selected\n",
+					argv[0]);
 				return 1;
 			} else {
 				flags.decrypt = 1;
 			}
-		} else if (is_key_option(*arg_ptr)) {
+			break;
+		case KEY:
 			if (flags.key_arg) {
-				fprintf(stderr, "%s: invalid use: multiple keys detected\n", argv[0]);
+				fprintf(stderr,
+					"%s: invalid use: multiple keys detected\n",
+					argv[0]);
 				return 1;
 			} else if (--argc <= 1) {
-				fprintf(stderr, "%s: invalid use: key option selected but no key provided\n", argv[0]);
+				fprintf(stderr,
+					"%s: invalid use: key option selected but no key provided\n",
+					argv[0]);
 				return 1;
 			} else {
 				++arg_ptr;
-				if (strlen(*arg_ptr) > MAXKEYSTRLEN) {
-					fprintf(stderr, "%s: invalid use: key too long, max length: %d\n", argv[0], MAXKEYLEN);
+				if (strlen(*arg_ptr) > MAXKEYLEN) {
+					fprintf(stderr,
+						"%s: invalid use: key too long, max length: %d\n",
+						argv[0], MAXKEYLEN);
 					return 1;
 				} else {
 					flags.key_arg = 1;
 					strcpy(key, *arg_ptr);
 				}
 			}
-		} else {
-			fprintf(stderr, "%s: invalid argument: %s\n", argv[0], *arg_ptr);
+			break;
+		default:
+			fprintf(stderr,
+				"%s: invalid argument: %s\n",
+				argv[0], *arg_ptr);
 			return 1;
+			break;
 		}
 	}
 
 	if (!flags.key_arg) {
 		if (!get_key(key)) {
-			fprintf(stderr, "%s: error: cannot read key from key.txt\n", argv[0]);
+			fprintf(stderr,
+				"%s: error: cannot read key from key.txt\n",
+				argv[0]);
 			return 1;
 		}
 	}
@@ -75,7 +102,8 @@ int main(int argc, char **argv) {
 	} else if (flags.decrypt) {
 		// decrypt input
 	} else {
-		printf("%s: invalid use: neither encrypt (-e) nor decrypt (-d) selected\n", argv[0]);
+		printf("%s: invalid use: neither encrypt (-e) nor decrypt (-d) selected\n",
+			argv[0]);
 		return 1;
 	}
 
@@ -92,6 +120,13 @@ int is_decrypt_option(char *str) {
 
 int is_key_option(char *str) {
 	return strcmp(str, "-k") == 0 || strcmp(str, "--key") == 0;
+}
+
+char option_type(char *str) {
+	if (is_encrypt_option(str)) return ENCRYPT;
+	else if (is_decrypt_option(str)) return DECRYPT;
+	else if (is_key_option(str)) return KEY;
+	else return INVALID;
 }
 
 char *get_key(char *dest) {
