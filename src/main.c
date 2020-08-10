5099a0707bd146b413a1159d2e8c743f2b1886d6
foggynight <foggynight709@gmail.com>
2020-08-10 01:25:52 -0230

    Improve program command line arguments
    Improved the handling of command line arguments. Allowing them to be
    input in any order, rather than statically reading the second argument
    as the program's 'mode'.
    
    Also added the option to provide an encryption key from the command line
    rather than from the key.txt file.

--- src/main.c
+++ src/main.c
@@ -10,27 +10,69 @@
 
 int is_encrypt_option(char *str);
 int is_decrypt_option(char *str);
-char *get_key(void);
+int is_key_option(char *str);
+char *get_key(char *dest);
+
+struct Flags {
+	unsigned int encrypt : 1;
+	unsigned int decrypt : 1;
+	unsigned int key_arg : 1;
+} flags;
+
+char key[MAXSTRLEN];
 
 int main(int argc, char **argv) {
-	if (argc < 2) {
-		printf("%s: invalid use: too few arguments\n", argv[0]);
-		return 1;
-	} else if (argc > 2) {
-		printf("%s: invalid use: too many arguments\n", argv[0]);
-		return 1;
+	for (char **arg_ptr = argv+1; argc > 1; --argc, ++arg_ptr) {
+		if (is_encrypt_option(*arg_ptr)) {
+			if (flags.decrypt) {
+				printf("%s: invalid use: encrypt and decrypt selected\n", argv[0]);
+				return 1;
+			} else {
+				flags.encrypt = 1;
+			}
+		} else if (is_decrypt_option(*arg_ptr)) {
+			if (flags.encrypt) {
+				printf("%s: invalid use: encrypt and decrypt selected\n", argv[0]);
+				return 1;
+			} else {
+				flags.decrypt = 1;
+			}
+		} else if (is_key_option(*arg_ptr)) {
+			if (flags.key_arg) {
+				printf("%s: invalid use: multiple keys detected\n", argv[0]);
+				return 1;
+			} else if (--argc <= 1) {
+				printf("%s: invalid use: key option selected but no key provided\n", argv[0]);
+				return 1;
+			} else {
+				++arg_ptr;
+				if (strlen(*arg_ptr) > MAXSTRLEN) {
+					printf("%s: invalid use: key too long, max length: %d\n", argv[0], MAXKEYLEN);
+					return 1;
+				} else {
+					flags.key_arg = 1;
+					strcpy(key, *arg_ptr);
+				}
+			}
+		} else {
+			printf("%s: invalid argument: %s\n", argv[0], *arg_ptr);
+			return 1;
+		}
+	}
+
+	if (!flags.key_arg) {
+		get_key(key);
 	}
 
 	FILE *input_fp = stdin;
 	FILE *output_fp = stdout;
-	char *key = get_key();
-
-	if (is_encrypt_option(argv[1])) {
+	if (flags.encrypt) {
 		// encrypt input
-	} else if (is_decrypt_option(argv[1])) {
+	} else if (flags.decrypt) {
 		// decrypt input
 	} else {
-		printf("%s: invalid argument: %s\n", argv[0], argv[1]);
+		printf("%s: invalid use: neither encrypt (-e) nor decrypt (-d) selected\n", argv[0]);
+		return 1;
 	}
 
 	return 0;
@@ -44,13 +86,16 @@ int is_decrypt_option(char *str) {
 	return strcmp(str, "-d") == 0 || strcmp(str, "--decrypt") == 0;
 }
 
-char *get_key(void) {
+int is_key_option(char *str) {
+	return strcmp(str, "-k") == 0 || strcmp(str, "--key") == 0;
+}
+
+char *get_key(char *dest) {
 	FILE *key_file = fopen("key.txt", "r");
 	if (!key_file) return NULL;
 
-	char *key = calloc(MAXSTRLEN, sizeof(char));
-	fscanf(key_file, "%" str(MAXKEYLEN) "s", key);
+	fscanf(key_file, "%" str(MAXKEYLEN) "s", dest);
 	
 	fclose(key_file);
-	return key;
+	return dest;
 }
