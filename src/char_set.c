28e157885348f4df642fffdef911f73065527c0c
foggynight <foggynight709@gmail.com>
2020-08-12 00:02:58 -0230

    Fix various character set bugs
    Fix: update char_set macros
    
    Previously, the create_character_set macro always created a set with the
    name char_set. Added a macro argument to allow the character set to be
    given a different name.
    
    Fix: get_char_index macro bug
    
    The call to bin_search in the get_char_index macro did not pass any
    arguments for the left and right parameters.
    
    The solution was to pass 0 and CHARSETLEN-1 to the macro, representing
    the first and last characters of the char_set.
    
    Fix: missing character char_set bug
    
    Previously the create_char_set function skipped the blank character when
    filling the character set.
    
    The solution was to adjust the indexing of the for loop which filled the
    the array.

--- src/char_set.c
+++ src/char_set.c
@@ -2,7 +2,7 @@
 
 void create_char_set(char *char_set) {
 	char_set[0] = '\n';
-	for (int i = 1; i < CHARSETLEN; ++i) {
-		char_set[i] = ' ' + i;		
+	for (int i = 0; i < CHARSETLEN-1; ++i) {
+		char_set[i+1] = ' ' + i;
 	}
 }
--- src/char_set.h
+++ src/char_set.h
@@ -2,7 +2,7 @@
 
 #define CHARSETLEN	'~' - ' ' + 2
 
-#define get_char_set() char char_set[CHARSETLEN]; create_char_set(char_set);
-#define get_char_index(x) (char)(bin_search(char_set, (x)))
+#define get_char_set(x) char (x)[CHARSETLEN]; create_char_set((x));
+#define get_char_index(x, y) (char)(bin_search((x), (y), 0, CHARSETLEN-1))
 
 void create_char_set(char *char_set);
