b262651882eb6f2b92a327de985477fe66b30c57
foggynight <foggynight709@gmail.com>
2020-11-15 04:28:13 -0330

    Bugfix: Wrap macro in parenthesis
    Bug:
    The expansion of the KEYSTRLEN macro in config.h was not wrapped in
    parenthesis. Although the macro is unlikely to be used in an expression,
    better to be safe than sorry.
    
    Fix:
    Wrap the KEYSTRLEN expansion in parenthesis.

--- src/config.h
+++ src/config.h
@@ -1,2 +1,2 @@
-#define MAXKEYLEN 1024        // Max length of encryption key
-#define KEYSTRLEN MAXKEYLEN+1 // Length of string to store encryption key
+#define MAXKEYLEN 1024          // Max length of encryption key
+#define KEYSTRLEN (MAXKEYLEN+1) // Length of string to store encryption key
