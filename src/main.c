16cf06f1cf9f229ea20236c0851443bbc9e9dc17
foggynight <foggynight709@gmail.com>
2020-12-13 20:40:46 -0330

    Remove SPDX license identifier

--- src/main.c
+++ src/main.c
@@ -1,10 +1,9 @@
-// SPDX-License-Identifier: GPL-2.0
-// Copyright (C) 2020 Robert Coffey
-/**
- * --- fogoff ---
+/* --- fogoff ---
  *
  * File encryption utility.
- */
+ *
+ * Copyright (C) 2020 Robert Coffey
+ * Licensed under the GNU GPLv2 */
 
 #include <stdio.h>
 #include <stdlib.h>
