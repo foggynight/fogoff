0ff834926772a393ddaec94706a2f9f23545f7ed
foggynight <foggynight709@gmail.com>
2021-01-07 07:58:57 -0330

    Remove carriage return from character set

--- src/char_set.c
+++ src/char_set.c
@@ -7,7 +7,7 @@
 
 void create_char_set(char *char_set)
 {
-    strcpy(char_set, "\n\r\t");
+    strcpy(char_set, "\t\n");
     for (int i = 0; i < CHARSETLEN-3; ++i)
         char_set[i+3] = ' ' + i;
 }
