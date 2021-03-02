b3ac7c915838dd6189d9f3fa6d09f06a5e9e2898
foggynight <foggynight709@gmail.com>
2021-03-02 06:56:44 -0330

    Add main function

---
+++ src/main.c
@@ -0,0 +1,13 @@
+/**
+ * fogoff - File encryption utility
+ *
+ * Encrypt files using the Advanced Encryption Standard (AES) algorithm.
+ *
+ * Copyright (C) 2020-2021 Robert Coffey
+ * Released under the MIT license
+ **/
+
+int main(int argc, char **argv)
+{
+    return 0;
+}
