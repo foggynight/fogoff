ec36c16f53f5e2ef0bda992a3a08bae3e950eb03
foggynight <foggynight709@gmail.com>
2021-03-07 03:23:07 -0330

    Add rotate_word function

--- src/AES_key_schedule.c
+++ src/AES_key_schedule.c
@@ -4,3 +4,15 @@
  * Copyright (C) 2021 Robert Coffey
  * Released under the MIT license
  **/
+
+#include <stdint.h>
+
+/**
+ * Rotate a 32-bit word to the left by 8-bits.
+ *
+ * EXAMPLE: 0x12345678 -> 0x34567812
+ **/
+uint32_t rotate_word(uint32_t word)
+{
+    return (word << 8) | (word >> 24);
+}
